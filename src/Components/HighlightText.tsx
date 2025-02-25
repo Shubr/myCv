ac3fd01fe1b177bac6280f6b4d9486b155f2023d
Shubham Singh Rangra <shubhamrangra80@gmail.com>
2025-02-25 20:56:25 +1300

    fix

--- src/Components/HighlightText.tsx
+++ src/Components/HighlightText.tsx
@@ -3,12 +3,10 @@ import React from "react";
 interface HighlightProps {
   text: String;
   color?: string;
-  size?: string;
-  backgroundColor?: string;
 }
 
-const Highlight: React.FC<HighlightProps> = ({ text, backgroundColor }) => {
-  return <span style={{ backgroundColor: backgroundColor }}>{text}</span>;
+const Highlight: React.FC<HighlightProps> = ({ text, color }) => {
+  return <span style={{ backgroundColor: color }}>{text}</span>;
 };
 
 export default Highlight;
