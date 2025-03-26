57765128731120e27f5cb066d623d198287b864f
Shub <shubhamrangra80@gmail.com>
2025-03-27 00:36:36 +1300

    master olli advice

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./Pages/MainPage";
 import TestMe from "./Pages/TestMe";
+import { StrictMode } from "react";
 
 function App() {
   return (
-    <>
+    <StrictMode>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/TestMe" element={<TestMe />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </StrictMode>
   );
 }
 
