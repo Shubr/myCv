1b4277bffbd728e89255b17e3137e8154e330895
Shub <shubhamrangra80@gmail.com>
2025-12-15 13:39:00 +1300

    fetching token from server

--- src/Pages/games/meow.tsx
+++ src/Pages/games/meow.tsx
@@ -1,28 +1,41 @@
 import { useEffect } from 'react';
 import '../Games.css';
-import getCat, { CatList } from './hooks/useMeow';
+// import getCat, { CatList } from './hooks/useMeow';
 import './hooks/useMeow';
 
 
 
 export default function Meow(){
     useEffect(()=>{
-        const container = document.getElementById("main-container")
-        let click = 0;
+        // const container = document.getElementById("main-container")
+        // let click = 0;
 
         async function getCats(){
-            const json = await getCat()
-            const data: CatList = json
-            const urls = data.map(cats=>cats.url)
-            preloadImage(urls)
-            if(container){
-                // container.addEventListener("click", )
-                container.addEventListener("click", (e)=>{
-                    if(click >= urls.length) return;
-                    addCat(e.clientX.toString(), e.clientY.toString(), urls[click])
-                    click+=1
-                })
-            }
+            // const json = await getCat()
+            // const data: CatList = json
+            // const urls = data.map(cats=>cats.url)
+            // preloadImage(urls)
+            // if(container){
+            //     // container.addEventListener("click", )
+            //     container.addEventListener("click", (e)=>{
+            //         if(click >= urls.length) return;
+            //         addCat(e.clientX.toString(), e.clientY.toString(), urls[click])
+            //         click+=1
+            //     })
+            // }
+            console.log("loading...")
+            const tokenRes = await fetch("https://shub.website/meow_token/api/key");
+            const {token} = await tokenRes.json()
+
+            const keyRes = await fetch(
+                "https://shub.website/meow_token/api/key",{
+                    headers:{
+                        "X-Site-Token": token
+                    }
+                }
+            )
+            const data = await keyRes.json();
+            console.log(data)
         }
 
         getCats();
@@ -43,24 +56,24 @@ export default function Meow(){
     )
 }
 
-function addCat(x:String, y:String, img: string){
-    const main_container = document.getElementById("main-container")
-    const text = document.createElement("img");
-    text.src = img;
-    text.style.position = "absolute"
-    text.className = "cat-image"
-    text.style.left = x+"px"
-    text.style.top = y+"px"
+// function addCat(x:String, y:String, img: string){
+//     const main_container = document.getElementById("main-container")
+//     const text = document.createElement("img");
+//     text.src = img;
+//     text.style.position = "absolute"
+//     text.className = "cat-image"
+//     text.style.left = x+"px"
+//     text.style.top = y+"px"
       
-    if(main_container){
-        main_container.appendChild(text)
-    }
+//     if(main_container){
+//         main_container.appendChild(text)
+//     }
 
-}
+// }
 
-function preloadImage(urls:string[]){
-    urls.forEach(url=>{
-        const img = new Image();
-        img.src = url
-    })
-}
+// function preloadImage(urls:string[]){
+//     urls.forEach(url=>{
+//         const img = new Image();
+//         img.src = url
+//     })
+// }
